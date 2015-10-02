c1573948a76d6bc595d5547999c6cb857bea3908
EvilNinjaUnicorn <Em.Hubenschmid@hotmail.de>
2015-10-03 00:12:38 +0200

    Login

--- login.php
+++ login.php
@@ -1,8 +1,7 @@
  <?php
     session_start();
-?>
 
-<?php
+   
     $verbindung = mysql_connect("localhost", "root" , "1234.abcd")
         or die("Verbindung zur Datenbank konnte nicht hergestellt werden");
     mysql_select_db("test") or die ("Datenbank konnte nicht ausgewählt werden");
@@ -13,18 +12,17 @@
     $abfrage = "SELECT Username, Password FROM Login WHERE Username LIKE '$username' LIMIT 1";
     $ergebnis = mysql_query($abfrage);
     $row = mysql_fetch_object($ergebnis);
-
-
+  
     if($row->Password == $passwort)
-        {
+    {
         $_SESSION["username"] = $username;
         echo "Login erfolgreich. <br> <a href=\"profile.html\">Zum Profiel</a>";
-        }
+    }
     else
-        {
+    {
         echo "Benutzername und/oder Passwort waren falsch. <a href=\"index.html\">Login</a>";
-        }
-
-    session_register_shutdown();
+    }
+    
+    
 
 ?> 
