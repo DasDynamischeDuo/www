6df3ad2c288a5d29db307f6a775da4c67c3a5383
EvilNinjaUnicorn <Em.Hubenschmid@hotmail.de>
2015-10-02 22:25:25 +0200

    Star building login

---
+++ profile.php
@@ -0,0 +1,16 @@
+<?php
+        session_start();
+?>
+
+
+<?php
+        $username = $_SESSION["username"];
+
+        $connection = mysql_connect("localhost", "root", "1234.abcd");
+        $db = mysql_select_db("test");
+
+        $query = "SELECT * FROM Login WHERE Username = " . $username . ";"
+        $ergebnis = mysql_query($abfrage);
+        print $ergebnis;
+?>
+            
